df06c3eefe9e1dccc1bbb54c51f4aa0f559505e0
woodwardtw <twwoodward@me.com>
2022-08-19 15:26:54 -0400

    better nav shift on scroll

--- js/dlinq-ssr-main.js
+++ js/dlinq-ssr-main.js
@@ -3,19 +3,23 @@ console.log('nice to be needed')
 
 window.onload = function() {
 	if(document.querySelector('#ssr-b-nav')){
-		const footer = document.querySelector('#footer');
+		const footerY = footer.getBoundingClientRect().y;
 		const footerNav = document.querySelector('#ssr-b-nav');
-		document.addEventListener("scroll", (event) => {
-		  const yHeight = window.scrollY;
-		  const footerHeight = footer.getBoundingClientRect().height;
-		  const footerY = footer.getBoundingClientRect().y;
-		  if(footer.getBoundingClientRect().y < footerNav.getBoundingClientRect().y + footer.getBoundingClientRect().height){
-		     footerNav.style.bottom = footerHeight+'px'
-		     }
-		  if(footer.getBoundingClientRect().y > footerNav.getBoundingClientRect().y + footer.getBoundingClientRect().height){
-		     footerNav.style.bottom = 0+'px'
-		     }
-		});
-
+		const footerHeight = footer.getBoundingClientRect().height;
+		const body = document.body;
+		const html = document.documentElement;
+		const pageHeight = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
+	
+		window.onscroll = function(ev) {
+			const footerNav = document.querySelector('#ssr-b-nav');
+		    if ((window.innerHeight + window.pageYOffset) >= pageHeight-footerHeight) {
+		          const moveIt = (window.innerHeight + window.pageYOffset)-(pageHeight-footerHeight);
+		       console.log((window.innerHeight + window.pageYOffset)-(pageHeight-footerHeight));
+		       footerNav.style.bottom = moveIt+'px';
+		    }
+		    if ((window.innerHeight + window.pageYOffset) < pageHeight-footerHeight) {
+		    	footerNav.style.bottom = '0px';
+		    }
+		};
 	}
 }
